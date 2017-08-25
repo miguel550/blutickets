3cbaa3f75debbaf29bc2fdaf74d61176e98cfda0
Miguel <miguel.almonte1@gmail.com>
2017-08-24 23:05:26 -0400

    Add media folder to staticdirs

--- blutickets/settings.py
+++ blutickets/settings.py
@@ -134,6 +134,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'media'),
 )
 
 MEDIA_URL = '/media/'

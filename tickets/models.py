9d6281c20bf6fca420adfbe19a98e41b4445ad7a
Miguel <miguel.almonte1@gmail.com>
2017-08-24 22:57:27 -0400

    Add timestamp to ticket model and when datetimefield

--- tickets/models.py
+++ tickets/models.py
@@ -4,8 +4,11 @@ from django.db import models
 class Ticket(models.Model):
     party_name = models.CharField(max_length=150)
     flyer_image = models.ImageField()
-    direction = models.CharField(max_length=250)
+    address = models.CharField(max_length=250)
     description = models.TextField()
+    when = models.DateTimeField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.party_name

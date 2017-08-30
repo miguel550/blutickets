435e33134606f0fb9427755a47fd58fd54d0769f
Miguel <miguel.almonte1@gmail.com>
2017-08-30 16:09:07 -0400

    Temp fix

--- blutickets/urls.py
+++ blutickets/urls.py
@@ -32,7 +32,7 @@ urlpatterns = [
 ]
 
 
-if settings.DEBUG:
+if False:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     import debug_toolbar

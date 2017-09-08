fd875b929295b7a23be5a5a69d131f4fb901e856
Miguel <miguel.almonte1@gmail.com>
2017-09-08 04:20:59 -0400

    Modify admin page to show only items and status and outside show address

--- sales/admin.py
+++ sales/admin.py
@@ -1,5 +1,20 @@
 from django.contrib import admin
-from .models import Order
+from .models import Order, LineItem
 
-admin.site.register(Order)
 
+class TermInlineAdmin(admin.TabularInline):
+    model = Order.line_items.through
+
+
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('status', 'show_address', 'user', )
+    fields = ('status',)
+
+    inlines = (TermInlineAdmin,)
+
+    def show_address(self, obj):
+        if obj.address:
+            return f"{obj.address.sector.name}, {obj.address.street_and_house}, {obj.address.reference}"
+        return "No address."
+
+admin.site.register(Order, OrderAdmin)

e9783e2acf9b1c8d281510671f95276d4031a5e0
Miguel <miguel.almonte1@gmail.com>
2017-09-12 07:14:54 -0400

    Set jet admin page template

--- blutickets/urls.py
+++ blutickets/urls.py
@@ -25,6 +25,8 @@ from sales.views import (create_order, edit_order_and_next,
 
 
 urlpatterns = [
+    url(r'^jet/', include('jet.urls', 'jet')),
+    url(r'^jet/dashboard/', include('jet.dashboard.urls', 'jet-dashboard')),
     url(r'^admin/', admin.site.urls),
     url(r'^$', TicketList.as_view(), name='home'),
     url(r'^accounts/', include('allauth.urls')),

de98b9b08fbacf9543b77db3e4fbcd6d97d2d254
Miguel <miguel.almonte1@gmail.com>
2019-06-04 04:16:05 -0400

    Add MapPosition graphene object to represent map_position field in model

--- tickets/schema.py
+++ tickets/schema.py
@@ -4,12 +4,22 @@ from .models import Ticket
 from graphql_relay.node.node import to_global_id
 
 
+class MapPosition(graphene.ObjectType):
+    lat = graphene.Float()
+    long = graphene.Float()
+
+
 class TicketType(graphene_django.DjangoObjectType):
+    map_position = graphene.Field(MapPosition)
+
     class Meta:
         model = Ticket
 
     def resolve_id(self, info):
         return to_global_id(self.__class__.__name__, self.id)
+
+    def resolve_map_position(self, info):
+        return MapPosition(lat=float(self.map_position.latitude), long=float(self.map_position.longitude))
 
 
 class Query:
